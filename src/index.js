eb6169325f89dd0f2bb6096c0393c5ad862498fe
newyorkgs <104804400+newyorkgs@users.noreply.github.com>
2024-03-13 18:05:52 -0400

    Update

--- src/index.js
+++ src/index.js
@@ -55,6 +55,48 @@ function handleSearchSubmit(event) {
     searchCity(searchInput.value);
 }
 
+function formatDay(timestamp) {
+    let date = new Date(timestamp * 1000);
+    let days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
+    return days[date.getDay()];
+}
+
+function getForecast(city) {
+    let apiKey = 't073a4e6ab40off0ea6fbe4dbf5579';
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+    axios(apiUrl).then(displayForecast);
+}
+
+function displayForecast(response) {
+    let forecastHtml = '';
+
+    response.data.daily.forEach(function (day, index) {
+        if (index < 5) {
+            forecastHtml =
+                forecastHtml +
+                `
+      <div class="weather-forecast-day">
+        <div class="weather-forecast-date">${formatDay(day.time)}</div>
+
+        <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
+        <div class="weather-forecast-temperatures">
+          <div class="weather-forecast-temperature">
+            <strong>${Math.round(day.temperature.maximum)}º</strong>
+          </div>
+          <div class="weather-forecast-temperature">${Math.round(
+              day.temperature.minimum
+          )}º</div>
+        </div>
+      </div>
+    `;
+        }
+    });
+
+    let forecastElement = document.querySelector('#forecast');
+    forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector('#search-form');
 //is finding the search form on the webpage. It's like telling the computer to look for something with the class name 'search-form' in the webpage.
 searchFormElement.addEventListener('submit', handleSearchSubmit);
